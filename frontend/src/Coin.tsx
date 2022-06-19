fd546e885e89507eb8f015c890b3e325e4390e75
Miles Kwok <dev@mileskmg.work>
2022-06-20 01:12:24 +0800

    add additional style for 'change'

--- frontend/src/Coin.tsx
+++ frontend/src/Coin.tsx
@@ -8,13 +8,16 @@ interface CoinProps {
   
 }
 
-
 function Coin(props :CoinProps) {
 
  const capitalizeFirst = (str :string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
+ const stylesForChanges = {
+   color: Math.sign(props.change) === 1 ? "#97dd5e" : "#d64c59"
+ }
+  
     return (
         <div className="card">
 
@@ -26,8 +29,10 @@ function Coin(props :CoinProps) {
 
         <div className="coin-bottom">
 
-        <div className="coin-volume">volume: <br/> {props.volume.toFixed(5)}</div> 
-        <div className="coin-change">change: <br/> {props.change.toFixed(5)}</div>
+      <div className="coin-volume"> volume: <br/>   {props.volume.toFixed(4)}</div> 
+        <div className="coin-change" 
+         style={stylesForChanges} 
+        >change: <br/> {props.change.toFixed(5)}%</div>
 
         </div> 
        
